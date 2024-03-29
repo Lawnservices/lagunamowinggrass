7a44946a4da94591d42317324ffa4a11ce07bb52
miguel <80590513+Lawnservices@users.noreply.github.com>
2024-03-29 12:55:32 -0400

    fix-web

--- titulo.js
+++ titulo.js
@@ -6,3 +6,18 @@ myPromise.then(function(value) {
   document.getElementById("place").innerHTML = value;
   document.getElementById("llamar").innerHTML= "240-883-8479";
 });
+
+document.getElementById('video').addEventListener('click',function(){
+document.getElementById('video').style.display = 'none';
+document.getElementById('videoss').style.display = 'block';
+document.getElementById('videos').style.display = 'block';
+document.getElementById('titulo').style.display = 'none';
+ 
+});
+
+document.getElementById('videoss').addEventListener('click',function(){
+document.getElementById('videoss').style.display = 'none';
+document.getElementById('video').style.display = 'block';
+document.getElementById('videos').style.display = 'none';
+document.getElementById('titulo').style.display = 'block';
+});
